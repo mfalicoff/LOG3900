6f972afa1eeb55368bb1bb953e7af84b5933386c
CorentinGlaus <glaus.corentin@gmail.com>
2022-09-29 03:03:41 -0400

    now scrolls on messages

--- client_leger/lib/chat_page.dart
+++ client_leger/lib/chat_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:client_leger/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
@@ -32,6 +34,7 @@ class _MyChatPageState extends State<ChatPage> {
   var msgController = TextEditingController();
 
   final FocusNode _focusNode = FocusNode();
+  final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
@@ -42,6 +45,9 @@ class _MyChatPageState extends State<ChatPage> {
             .build());
 
     initSockets();
+    _focusNode.addListener(() {
+      scrollMessages();
+    });
     super.initState();
   }
 
@@ -53,7 +59,9 @@ class _MyChatPageState extends State<ChatPage> {
               msg: data['msg'],
               sender: data['sender'],
               timestamp: data['timestamp']));
+
         });
+        scrollMessages();
       }
     });
   }
@@ -63,147 +71,159 @@ class _MyChatPageState extends State<ChatPage> {
     final user = ModalRoute.of(context)!.settings.arguments as User;
 
     return Scaffold(
-      appBar: AppBar(),
+        resizeToAvoidBottomInset: true,
+        appBar: AppBar(),
         body: (Stack(
-      children: <Widget>[
-        Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/background.jpg"),
-              fit: BoxFit.cover,
+          children: <Widget>[
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/background.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: ListView.builder(
-            itemCount: chatHistory.length,
-            shrinkWrap: true,
-            padding: const EdgeInsets.only(top: 10, bottom: 10),
-            //physics: const NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              return Column(
-                crossAxisAlignment: chatHistory[index].sender == user.username
-                    ? CrossAxisAlignment.end
-                    : CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 3, 10, 3),
-                    child: Text(
-                      chatHistory[index].sender,
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.primary,
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 60),
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: ListView.builder(
+                  itemCount: chatHistory.length,
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.only(top: 10, bottom: 10),
+                  controller: _scrollController,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      crossAxisAlignment:
+                          chatHistory[index].sender == user.username
+                              ? CrossAxisAlignment.end
+                              : CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 3, 10, 3),
+                          child: Text(
+                            chatHistory[index].sender,
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment:
+                              chatHistory[index].sender == user.username
+                                  ? MainAxisAlignment.end
+                                  : MainAxisAlignment.start,
+                          children: [
+                            Visibility(
+                              visible:
+                                  chatHistory[index].sender == user.username,
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                child: Text(
+                                  readableTime(chatHistory[index].timestamp),
+                                  style: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                    fontSize: 10.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Flexible(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color:
+                                      Theme.of(context).colorScheme.secondary,
+                                  border: Border.all(
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                    width: 2.0,
+                                  ),
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(18)),
+                                ),
+                                padding: const EdgeInsets.only(
+                                    left: 25, right: 25, top: 8, bottom: 8),
+                                child: Text(
+                                  chatHistory[index].msg,
+                                  style: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Visibility(
+                              visible:
+                                  chatHistory[index].sender != user.username,
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                child: Text(
+                                  readableTime(chatHistory[index].timestamp),
+                                  style: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                    fontSize: 10.0,
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
+                height: 60,
+                width: double.infinity,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        focusNode: _focusNode,
+                        controller: msgController,
+                        textInputAction: TextInputAction.send,
+                        onSubmitted: (value) {
+                          sendMessage(user);
+                          _focusNode.requestFocus();
+                        },
+                        decoration: InputDecoration(
+                          fillColor: Theme.of(context).colorScheme.secondary,
+                          filled: true,
+                          hintText: "Write message...",
+                          hintStyle: const TextStyle(color: Colors.black54),
+                          border: InputBorder.none,
+                        ),
+                        onChanged: (String value) {
+                          message = value;
+                        },
                       ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment:
-                        chatHistory[index].sender == user.username
-                            ? MainAxisAlignment.end
-                            : MainAxisAlignment.start,
-                    children: [
-                      Visibility(
-                        visible: chatHistory[index].sender == user.username,
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
-                          child: Text(
-                            readableTime(chatHistory[index].timestamp),
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.primary,
-                              fontSize: 10.0,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Flexible(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).colorScheme.secondary,
-                            border: Border.all(
-                              color: Theme.of(context).colorScheme.primary,
-                              width: 2.0,
-                            ),
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(18)),
-                          ),
-                          padding: const EdgeInsets.only(
-                              left: 25, right: 25, top: 8, bottom: 8),
-                          child: Text(
-                            chatHistory[index].msg,
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Visibility(
-                        visible: chatHistory[index].sender != user.username,
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                          child: Text(
-                            readableTime(chatHistory[index].timestamp),
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.primary,
-                              fontSize: 10.0,
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ],
-              );
-            },
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomLeft,
-          child: Container(
-            padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
-            height: 60,
-            width: double.infinity,
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    focusNode: _focusNode,
-                    controller: msgController,
-                    textInputAction: TextInputAction.send,
-                    onSubmitted: (value) {
-                      sendMessage(user);
-                      _focusNode.requestFocus();
-                    },
-                    decoration: InputDecoration(
-                      fillColor: Theme.of(context).colorScheme.secondary,
-                      filled: true,
-                      hintText: "Write message...",
-                      hintStyle: const TextStyle(color: Colors.black54),
-                      border: InputBorder.none,
+                    const SizedBox(
+                      width: 15,
                     ),
-                    onChanged: (String value) {
-                      message = value;
-                    },
-                  ),
+                    FloatingActionButton(
+                      onPressed: () => {sendMessage(user)},
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      elevation: 0,
+                      child: Icon(
+                        Icons.send,
+                        color: Theme.of(context).colorScheme.secondary,
+                        size: 18,
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(
-                  width: 15,
-                ),
-                FloatingActionButton(
-                  onPressed: () => {sendMessage(user)},
-                  backgroundColor: Theme.of(context).colorScheme.primary,
-                  elevation: 0,
-                  child: Icon(
-                    Icons.send,
-                    color: Theme.of(context).colorScheme.secondary,
-                    size: 18,
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ],
-    )));
+          ],
+        )));
   }
 
   void initPage() {
@@ -220,7 +240,9 @@ class _MyChatPageState extends State<ChatPage> {
         chatHistory.add(chat);
         message = "";
       });
+      scrollMessages();
       socket.emit("chat msg", chat);
+
     }
     message = "";
     msgController.clear();
@@ -228,5 +250,14 @@ class _MyChatPageState extends State<ChatPage> {
 
   bool isMessageEmpty() {
     return message.isEmpty || message.trim() == '';
+  }
+
+  void scrollMessages(){
+    Timer(const Duration(milliseconds: 50), () {
+      _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.fastOutSlowIn);
+    });
   }
 }
