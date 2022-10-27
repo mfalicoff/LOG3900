015d1f17d35b1ba93cd4af26004563720dc648e9
Maximiliano Falicoff <mfalicoff@yahoo.com>
2022-10-27 10:31:48 -0400

    added basic classes

---
+++ client_leger/lib/models/letter.dart
@@ -0,0 +1,9 @@
+class Letter {
+  late String value;
+  late num? weight;
+
+  Letter() {
+    value = '';
+    weight = 0;
+  }
+}
---
+++ client_leger/lib/models/tile.dart
@@ -0,0 +1,22 @@
+import 'package:client_leger/models/letter.dart';
+import 'package:client_leger/models/vec4.dart';
+
+class Tile {
+  late Vec4 position;
+  late Letter letter;
+  late String? bonus;
+  late bool old;
+  late String backgroundColor;
+  late String borderColor;
+  late bool isOnBoard;
+
+  Tile() {
+    position = Vec4();
+    letter = Letter();
+    bonus = '';
+    old = false;
+    backgroundColor = '#F7F7E3';
+    borderColor = '#212121';
+    isOnBoard = false;
+  }
+}
