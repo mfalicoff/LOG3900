abc034f80b5cd49dc214fe948c870bcc7ca86e8b
mohamedfenjiro1 <fenjiromohamed1@gmail.com>
2022-10-28 03:52:00 +0000

    Update client/src/environments/environment.ts

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    production: false,
-    serverUrl: 'http://localhost:3000/',
+    production: true,
+    serverUrl: 'ec2-15-222-9-242.ca-central-1.compute.amazonaws.com',
 };
 
 /*
