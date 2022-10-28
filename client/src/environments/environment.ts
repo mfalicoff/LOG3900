2bf6ec7afb2f8900bc2e3ca0c2fa7cfe14d8bf09
mohamedfenjiro1 <fenjiromohamed1@gmail.com>
2022-10-28 04:10:35 +0000

    Update client/src/environments/environment.ts

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: true,
-    serverUrl: 'ec2-15-222-9-242.ca-central-1.compute.amazonaws.com',
+    serverUrl: 'http://ec2-15-222-9-242.ca-central-1.compute.amazonaws.com:3000/',
 };
 
 /*
