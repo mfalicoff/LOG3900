4907a10bfd7e4a5eb360fc87fde713942297316e
Maximiliano Falicoff <maximiliano.falicoff@polymtl.ca>
2022-10-31 15:24:21 +0000

    DRag init and bug fixes

--- client_leger/lib/models/vec2.dart
+++ client_leger/lib/models/vec2.dart
@@ -6,4 +6,16 @@ class Vec2 {
     x = 0;
     y = 0;
   }
+
+  Vec2.withParams(this.x, this.y);
+
+  Vec2.fromJson(Map parsed) {
+    x = parsed["x"];
+    y = parsed["y"];
+  }
+
+  Map<String, dynamic> toJson() => {
+    'x': x,
+    'y': y,
+  };
 }
