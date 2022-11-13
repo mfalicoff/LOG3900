b1c0f3c6a51742be2485bd48d3156d1101238b90
Stéphane Toyo Demanou <stephanetoyo14@gmail.com>
2022-11-12 19:51:35 -0500

    Working on end game results pop up layout

--- client_leger/lib/screens/end-game-results-page.dart
+++ client_leger/lib/screens/end-game-results-page.dart
@@ -6,19 +6,67 @@ import 'package:client_leger/models/player.dart';
 import 'package:client_leger/models/game-saved.dart';
 import 'package:flutter/material.dart';
 
-class EndGameResultsPage extends StatelessWidget{
-    const EndGameResultsPage({Key? key}) : super(key: key);
+
+class EndGameResultsPage extends StatefulWidget {
+  const EndGameResultsPage({Key? key}) : super(key: key);
+
+  @override
+  State<EndGameResultsPage> createState() => _EndGameResultsPage();
+}
+class _EndGameResultsPage extends State<EndGameResultsPage> {
+    final InfoClientService infoClientService = InfoClientService();
+    String _creator = "Stephane";
+    List<String> spectators = ["Lisa", "Varys"];
+    List<String> players = ["Petyr", "Robert", "Cersei", "Eddard"];
+    List<String> winners = ["Cersei", "Petyr"];
 
     @override
     Widget build(BuildContext context) {
-        return SimpleDialog(
-          title: const Text('RESULTATS DE FIN DE PARTIE'),
-          children: <Widget>[
-            SimpleDialogOption(
-              onPressed: () { Navigator.pop(context, 'Ajouter partie aux favoris'); },
-              child: const Text('Treasury department'),
-            )
-          ],
+        return Dialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+          backgroundColor: Theme.of(context).colorScheme.secondary,
+          insetPadding: const EdgeInsets.all(90.0),
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+                Text("RESULTATS DE FIN DE PARTIE",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge,
+                ),
+
+                Text("Createur de la salle : $_creator",
+                    textAlign: TextAlign.left,
+                    selectionColor: Theme.of(context).colorScheme.secondary,
+                    style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                ListView.builder(
+                    itemBuilder:(context, index) =>
+                    Text("Nom: $index"),
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: 10,
+                )
+
+            ],
+          )
+        //   child: Stack(
+        //     children: [
+        //         Center(
+        //             child: Column(
+        //                 verticalDirection: VerticalDirection.down,
+        //                 crossAxisAlignment: CrossAxisAlignment.center,
+        //                 children: [
+        //                     Text(
+        //                         "RESULTATS DE FIN DE PARTIE",
+        //                         style: Theme.of(context).textTheme.titleLarge,
+
+        //                     )
+        //                 ],
+        //             )
+        //         ),
+        //     ],
+        //   )
+
+
         );
     }
 }
