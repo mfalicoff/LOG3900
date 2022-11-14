6fad87ec399783c74793adbbf3272fbdf0b869be
unknown <marcantoineb00@gmail.com>
2022-11-14 11:12:27 -0500

    player joins the ranked game page

--- client_leger/lib/screens/ranked_matchmaking_page.dart
+++ client_leger/lib/screens/ranked_matchmaking_page.dart
@@ -34,6 +34,15 @@ class _RankedMatchmakingPageState extends State<RankedMatchmakingPage> {
     }
   }
 
+  void initSockets() {
+    socketService.socket.on("roomChangeAccepted", (data) {
+      if (mounted) {
+        Navigator.pushNamed(context, "/game");
+      }
+    });
+    socketService.socket.emit("listRoom");
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
