5e5d05d6a6d63c2e3dd589284a69eccafd711efb
marc <marcantoineb00@gmail.com>
2022-12-02 17:45:10 -0500

    added log for the test of performance

--- client/src/app/pages/profile-page/profile-edit/profile-edit.component.ts
+++ client/src/app/pages/profile-page/profile-edit/profile-edit.component.ts
@@ -1,8 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
-import { UserService } from '@app/services/user.service';
 import { MatDialog } from '@angular/material/dialog';
 import { GalleryComponent } from '@app/components/gallery/gallery.component';
 import { SocketService } from '@app/services/socket.service';
+import { UserService } from '@app/services/user.service';
 
 @Component({
     selector: 'app-profile-edit',
@@ -25,7 +25,9 @@ export class ProfileEditComponent {
     }
 
     async updateAvatar() {
+        console.time("1");
         await this.userService.updateAvatar(this.galleryComponent.ngxGalleryComponent.selectedIndex, this.socketService.socket);
+        console.timeEnd("1");
     }
 
     closeDialog() {
